6321c6c8a9a3cdc8489c8265c16a9f28ad9ca32b
WellingtonRuanDaSilva <163909213+WellingtonRuanDaSilva@users.noreply.github.com>
2024-05-17 14:51:06 -0300

    Base Pokemon, Tela de batalha
    Add Status Pokemons
    Add Pokemons
    Add Moves Pokemons

---
+++ Assets/Scripts/Pokemon/MoveBasePokemon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Move", menuName = "Pokemon/Create new move")]
+public class MoveBasePokemon : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] PokemonType type;
+    [SerializeField] int power;
+    [SerializeField] int accuracy;
+    [SerializeField] int pp;
+
+    public string Name => name;
+    public string Description => description;
+    public PokemonType Type => type;
+    public int Power => power;
+    public int Accuracy => accuracy;
+    public int PP => pp;
+}
---
+++ Assets/Scripts/Pokemon/PokemonBase.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")]
+
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    //Base Stats
+    [SerializeField] int maxHp;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+
+    [SerializeField] List<LearnableMove> learnableMoves;
+
+
+    //duas formas de expor variaveis privadas para outro codigo
+    // public string GetName()
+    //{
+    //    return name;
+    //}
+    //forma de propriedade (abaixo) transforma em variavel em vez de chamar uma função.
+    public string Name
+    {
+        get { return name; }
+    }
+    //mesma coisa 
+    //  public string Name => name;
+    public string Description
+    {
+        get { return description; }
+    }
+    public Sprite FrontSprite
+    {
+        get { return frontSprite; }
+    }
+    public Sprite BackSprite
+    {
+        get { return backSprite; }
+    }
+    public int MaxHp
+    {
+        get { return maxHp; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+    public int Defense
+    {
+        get { return defense; }
+    }
+    public int SpDefense
+    {
+        get { return spDefense; }
+    }
+    public int SpAttack
+    {
+        get { return spAttack; }
+    }
+    public int Speed
+    {
+        get { return speed; }
+    }
+    public List<LearnableMove> LearnableMoves
+    {
+        get { return learnableMoves; } 
+    }
+}
+
+[System.Serializable]
+public class LearnableMove
+{
+    [SerializeField] MoveBasePokemon moveBase;
+    [SerializeField] int level;
+
+    public MoveBasePokemon MoveBase => moveBase;
+    public int Level => level;
+}
+public enum PokemonType
+    {
+        None,
+        Normal,
+        Fire,
+        Water,
+        Eletric,
+        Grass,
+        Ice,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Psychic,
+        Bug,
+        Rock,
+        Ghost,
+        Dragon,
+    }
